b3f0ee63d1aa24a6cf30aad664e775bd03bd163f
Kaide Naohiro <kaide.jhh@gmail.com>
2025-05-27 07:35:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("requirements.txt", "r") as fh:
     requirements = fh.read()
 
 setup(
-    name='iglu-datasets',
+    name='iglu-datasets-modifeid',
     version='1.0',
     description='',
     long_description='',
     long_description_content_type="text/markdown",
-    url='https://github.com/microsoft/iglu-datasets',
-    author='Microsoft',
+    url='https://github.com/kidnohr2000/iglu-datasets',
+    author='kidnohr2000',
     author_email='',
     include_package_data=True,
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=requirements,
-    python_requires='>=3.7',
+    python_requires='>=3.12',
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
